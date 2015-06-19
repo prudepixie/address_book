767c17827fc71f996f8a967820c31b39a6fe5fa0
Wendy Hsu <wndyhsu@gmail.com>
2015-06-18 18:06:52 -0700

    replace hardcode with specific details

--- app/assets/javascripts/controllers/contacts_controller.js
+++ app/assets/javascripts/controllers/contacts_controller.js
@@ -1,4 +1,12 @@
 app.controller('ContactsCtrl', ['$scope', 'contactsService', function($scope, contactsService){
      $scope.contacts = contactsService.getContacts();
+     $scope.contactDetails = {}
+
+     $scope.showContactDetails = function(contact) {
+        $scope.contactDetails['name'] = contact.firstname + contact.lastname;
+        $scope.contactDetails['phone'] = contact.phone;
+        $scope.contactDetails['email'] = contact.email;
+        $scope.contactDetails['address'] = contact.address;
+     }
   }
 ]);
