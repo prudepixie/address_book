e954c555fe5e2a7a45b79c93afb7cfa18455999c
Wendy Hsu <wndyhsu@gmail.com>
2015-06-18 18:43:17 -0700

    add edit modal

--- app/assets/javascripts/controllers/contacts_controller.js
+++ app/assets/javascripts/controllers/contacts_controller.js
@@ -1,12 +1,19 @@
 app.controller('ContactsCtrl', ['$scope', 'contactsService', function($scope, contactsService){
      $scope.contacts = contactsService.getContacts();
-     $scope.contactDetails = {}
+     $scope.contactDetails = $scope.contacts[0];
+     $scope.toggleEditModal = false;
 
      $scope.showContactDetails = function(contact) {
-        $scope.contactDetails['name'] = contact.firstname + contact.lastname;
+        $scope.contactDetails['firstname'] = contact.firstname;
+        $scope.contactDetails['lastname'] = contact.lastname;
         $scope.contactDetails['phone'] = contact.phone;
         $scope.contactDetails['email'] = contact.email;
         $scope.contactDetails['address'] = contact.address;
-     }
+     };
+
+     $scope.editContact = function (name) {
+        $scope.toggleEditModal = true;
+     };
+
   }
 ]);
